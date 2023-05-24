18be402c84fe8a14371074cbb6c697f436e6d1da
Prithviraj <prithvirajvnair.pvr@gmail.com>
2023-05-24 12:17:28 +0530

    Making Changes for First Time

---
+++ src/components/ApiGet.jsx
@@ -0,0 +1,44 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const ApiGet = () => {
+    var [users,setUsers] = useState([]);
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{
+        setUsers(response.data)
+        console.log(response.data)
+    })
+    .catch(err=>console.log(err))
+    }) //Useeffect(for pop up(to avoid multiple rendering))
+    
+  return (
+    <div>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Username</TableCell>
+                        <TableCell>Email</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {users.map((value,index)=>{
+                    return(
+                        <TableRow>
+                            <TableCell>{value.name}</TableCell>
+                            <TableCell>{value.username}</TableCell>
+                            <TableCell>{value.email}</TableCell>
+                        </TableRow>
+                    )
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </div>
+  )
+}
+
+export default ApiGet
